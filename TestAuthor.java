334d6663eaf475b8ea8e258704b87c5ef1a04ebf
Luckylake215 <70714955+Luckylake215@users.noreply.github.com>
2020-09-15 12:49:43 +0300

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,6 @@
+public class TestAuthor {
+    public static void main(String[] args){
+        Author author =new Author("Pushkin","@pushkin.ru",'m');
+        System.out.println(author);
+    }
+}
